0a03789347ea203405b16fbae8be81490453d981
Harsh Bhotika <harshbhotika1512@gmail.com>
2017-07-10 01:55:07 -0400

    Update Problem 3.3.R

--- Problem 3.3.R
+++ Problem 3.3.R
@@ -1,7 +1,7 @@
 #Code written by Harsh Bhotika
 
-depress <- read.table(file.choose(), header = FALSE)
+depress <- read.csv(file.choose(), header = TRUE)
 View(depress)
 attach(depress)
-plot(V6,V7, main="Scatterplot",
+plot(EMPLOY,INCOME, main="Scatterplot",
      xlab="Employment Status ", ylab="Income in Thousand Dollars ", pch=1 , col='Red')
