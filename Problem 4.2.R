c1a469a2f89f27384e7ea8bf5a0cf6fc2223f107
Harsh Bhotika <harshbhotika1512@gmail.com>
2017-07-10 23:19:35 -0400

    Update Problem 4.2.R

--- Problem 4.2.R
+++ Problem 4.2.R
@@ -9,23 +9,23 @@ View(INCOME2)
 histogram1 <- hist(INCOME, main = "Histogram of Income",xlab = "Income" , border = "black" ,
                    col = "green", xlim = c(0,70), ylim = c(0,70))
 
-histogram2 <- hist(INCOME2, main = "Histogram of Log(Income)" , xlab = "Log(Income)"
-     , border = "black" , col = "red", ylim = c(0,60), xlim = c(0,2.0))
+histogram2 <- hist(INCOME2, main = "Histogram of Log(Income)" , xlab = "Log(Income)",
+                   border = "black" , col = "red", ylim = c(0,60), xlim = c(0,2.0))
 
-plot1 <- qqnorm(INCOME , col = "green" , main = "Normal Probability Plot of Income"
-                , ylab = 'Income')
+plot1 <- qqnorm(INCOME , col = "green" , main = "Normal Probability Plot of Income",
+                ylab = 'Income')
 qqline(INCOME)
 
-plot2 <- qqnorm(INCOME2 , col = "red" , main = "Normal Probability Plot of log(Income)"
-                , ylab = 'Log(Income)')
+plot2 <- qqnorm(INCOME2 , col = "red" , main = "Normal Probability Plot of log(Income)",
+                ylab = 'Log(Income)')
 qqline(INCOME2)
 
   par(mfrow=c(1,2))
   {
-  plot1 <- qqnorm(INCOME , col = "green" , main = "Probability Plot Income", ylab = 'Income' )
+  plot1 <- qqnorm(INCOME , col = "green" , main = "Probability Plot of Income", ylab = 'Income' )
   qqline(INCOME)
 
-  plot2 <- qqnorm(INCOME2 , col = "red" , main = "Probability Plot log(Income)"
+  plot2 <- qqnorm(INCOME2 , col = "red" , main = "Probability Plot of log(Income)"
                   , ylab = 'Log(Income)' )
   qqline(INCOME2)
 
