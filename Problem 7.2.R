0392120db2428c5f425654c81f8dbe2ac1a1185e
Harsh Bhotika <harshbhotika1512@gmail.com>
2017-07-14 22:02:32 -0400

    Add files via upload

---
+++ Problem 7.2.R
@@ -0,0 +1,20 @@
+# Code written by Harsh Bhotika
+
+lung <- read.csv(file.choose(), header = TRUE)
+View(lung)
+attach(lung)
+model <- (lm(FFVC~FAGE+FHEIGHT))
+summary(model)
+
+plot(FAGE,FFVC, col='Red', pch=20, main='Age vs FFVC for Fathers')
+abline(lm(FFVC~FAGE))
+plot(FHEIGHT,FFVC, col='Blue', pch=20, main = 'Height vs FFVC for Fathers')
+abline(lm(FFVC~FHEIGHT))
+
+newdata = lung[c(4,5,7)]   #Matrix scatterplot allows to view relationship among multiple variables in one graph
+plot(newdata)
+
+plot(model)
+
+cor(FFVC,FAGE)
+cor(FFVC,FHEIGHT)
